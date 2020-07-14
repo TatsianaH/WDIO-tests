fd520ac529d12d39e8a6731b11e4cc2e0ef7cc2f
TatsianaH <ourflsun@gmail.com>
2020-07-14 18:48:53 -0400

    add TC

--- usps/userRegistrationTest.js
+++ usps/userRegistrationTest.js
@@ -1,18 +1,26 @@
 import { expect } from 'chai';
-import {homePageD} from './data/data.json';
-import {homePage} from './data/selectors.json';
-import {signInPageD} from './data/data.json';
-import {signInPage} from './data/selectors.json';
+import { homePageD,signInPageD, registerPageD } from './data/data.json';
+import { homePage, signInPage, registerPage } from './data/selectors.json';
 
 describe('User Registration', () => {
     before('open home page usps', () => {
         browser.url(homePageD);
-
     });
 
     it('user should be redirected to `SignIn` page after `Register/SignIn` button was clicked', () => {
         $(homePage.registerSignInLink).click();
         const actual = $(signInPage.header).getText();
         expect(actual).eq(signInPageD.header);
+    });
+
+    it('user should be redirected to `Register` page after `Register` button was clicked', () => {
+        $(signInPage.registerLink).click();
+        const actual = $(registerPage.header).getText();
+        expect(actual).eq(registerPageD.header);
+    });
+
+    it('should verify `Register` page title', () => {
+        const actual = browser.getTitle();
+        expect(actual).eq(registerPageD.title);
     });
 });
