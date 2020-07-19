fa9878cda59360d0c64aaa9c3cf3e91ede095f62
TatsianaH <ourflsun@gmail.com>
2020-07-19 16:55:45 -0400

    add imports

--- usps/data/userRegistration_FucntionalTests.js
+++ usps/data/userRegistration_FucntionalTests.js
@@ -1,3 +1,9 @@
 import { expect } from 'chai';
 import { homePage, signInPage, registerPage } from './data/selectors.json';
 import { homePageUrl, registerPageE, signInPageE } from './data/expected.json';
+
+describe('', () => {
+    it('', () => {
+
+    });
+});
