5a84469cf2648398630bac748eedb41acc012ff9
TatsianaH <ourflsun@gmail.com>
2020-07-22 19:55:31 -0400

    first test

---
+++ aviasales/aviasales.js
@@ -0,0 +1,7 @@
+import { expec t} from 'chai';
+
+describe('Book a flight ticket', () => {
+    before('open home page usps', () => {
+        browser.url('https://www.aviasales.com/');
+    });
+});
