0e50602aac23508ce7eec2e8d8c58306b88d1c5d
TatsianaH <ourflsun@gmail.com>
2020-07-27 19:33:52 -0400

    create herokuAppTests

---
+++ herokuApp/herokuAppTests.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+
+describe('Testing different elements', () => {
+    before('open home page', () => {
+        browser.url('https://the-internet.herokuapp.com/');
+    });
+
+    it('', () => {
+        const links = $$('ul li a');
+        for(let i = 0; i < links.length; i++){
+            if(links[i].getText() === 'A/B Testing') {
+                links[i].click();
+                break;
+            }
+        }
+        const actualUrl = browser.getUrl();
+        const actualHeader = $('h3').getText();
+        const expectedHeader = ['A/B Test Variation 1', 'A/B Test Control'];
+        expect(actualUrl).eq('https://the-internet.herokuapp.com/abtest');
+        expect(expectedHeader).to.include(actualHeader);
+    });
+});
