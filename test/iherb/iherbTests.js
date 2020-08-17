ff0a8a12870dad047b226f8f20491c07e1ddfe4d
TatsianaH <ourflsun@gmail.com>
2020-08-17 19:09:27 -0400

    add test

--- test/iherb/iherbTests.js
+++ test/iherb/iherbTests.js
@@ -12,4 +12,20 @@ describe('Add some Best Sellers to Cart', () => {
         const actual = header.getText();
         expect(actual).eq('Best Sellers');
     });
+
+    it('should verify the `United States` region is presented', () => {
+        const region = $('.country-filter');
+        const actual = region.getValue();
+        const isDisplayed = region.isDisplayed();
+        expect(actual).eq('United States');
+        expect(isDisplayed).true;
+    });
+
+    it('should choose the second product from `Bath & Personal Care` department', () => {
+        const link = $('*=Bath');
+        link.click();
+        const items = $$('#section-Bath-Personal-Care .description');
+        expect(items).to.have.lengthOf(10);
+        items[1].click();
+    });
 });
