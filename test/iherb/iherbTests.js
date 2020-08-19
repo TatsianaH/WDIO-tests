239b8b1fd5ec398a9a788f0ef768d36ca2035336
TatsianaH <ourflsun@gmail.com>
2020-08-18 20:03:29 -0400

    add tests

--- test/iherb/iherbTests.js
+++ test/iherb/iherbTests.js
@@ -2,9 +2,12 @@ import { expect } from 'chai';
 
 let bathPrice = 0;
 let bathDescription = '';
+let numberOfProducts = 0;
 
 describe('Add some Best Sellers to Cart', () => {
     before(() => {
+        browser.deleteAllCookies();
+        browser.setWindowSize(1400, 900);
         browser.url('https://www.iherb.com/');
     });
 
@@ -27,13 +30,18 @@ describe('Add some Best Sellers to Cart', () => {
     it('should choose the second product from `Bath & Personal Care` department', () => {
         const link = $('*=Bath');
         link.click();
-        const items = $$('#section-Bath-Personal-Care .description .product-title');
+        const items = $$('#section-Bath-Personal-Care .description');
+        const itemsTitle = $$('#section-Bath-Personal-Care .description .product-title');
         const prices = $$('#section-Bath-Personal-Care .price');
-        bathDescription = items[1].getText();
+        bathDescription = itemsTitle[1].getText();
         bathPrice = prices[1].getText();
         expect(items).to.have.lengthOf(10);
         expect(prices).to.have.lengthOf(10);
         items[1].click();
+
+        const urlTopSellers = 'https://www.iherb.com/catalog/topsellers';
+        const urlNew = browser.getUrl();
+        browser.waitUntil(() => urlNew !== urlTopSellers, { timeout: 3000, timeoutMsg: 'No new page is displayed' });
     });
 
     it('should verify the user gets redirected to selected product page', () => {
@@ -41,4 +49,38 @@ describe('Add some Best Sellers to Cart', () => {
         const linkPartialExpected = bathDescription.split(' ')[0];
         expect(urlActual).to.include(linkPartialExpected);
     });
+
+    it('should verify the name and the price of the product are equal to information on the Best Sellers page', () => {
+        const header = $('.hidden-xs #product-summary-header h1');
+        const headerActual = header.getText();
+        const price = $('#price');
+        const priceActual = price.getText();
+        expect(headerActual).eq(bathDescription);
+        expect(priceActual).eq(bathPrice);
+    });
+
+    // it('should add 2 product (from the Bath department) to the cart', () => {
+    //     const quantity = $('#volume-quantity');
+    //     const btn = $('button[name="AddToCart"]');
+    //     const quantityValue = $$('#volume-quantity option');
+    //     numberOfProducts += +quantityValue[1].getAttribute('value');
+    //     quantity.selectByIndex(1);
+    //     btn.click();
+    //     const msg = $('#add-to-cart-popup');
+    //     browser.acceptAlert();
+    //     //msg.waitForDisplayed();
+    //     // const isOpen = browser.isAlertOpen();
+    //     //
+    //     // if (isOpen) {
+    //     //     console.log("===========>" + isOpen);
+    //     //     browser.acceptAlert();
+    //     //     browser.pause(5000);
+    //     // }
+    //     // const text = browser.getAlertText();
+    //     // console.log(text, '///////////////////');
+    //     // browser.waitUntil(() => msg.isDisplayed() === false, {
+    //     //     timeout: 2000,
+    //     //     timeoutMsg: 'No pop-up message is showed up',
+    //     // });
+    // });
 });
